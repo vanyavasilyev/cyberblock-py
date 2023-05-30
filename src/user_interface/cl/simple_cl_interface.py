9eb6f52b4296b52326d90cd9ee38a4567f317ba2
Ivan Vasilyev <vasilyevivan.spb@gmail.com>
2023-05-30 21:11:17 +0300

    exception-save mode

--- src/user_interface/cl/simple_cl_interface.py
+++ src/user_interface/cl/simple_cl_interface.py
@@ -2,13 +2,30 @@ from models import AnalyzerInterface
 
 
 class CLInterface:
-    def __init__(self, analyzer: AnalyzerInterface) -> None:
+    def __init__(self, analyzer: AnalyzerInterface, safe=True) -> None:
         self.analyzer = analyzer
+        self.safe = safe
 
     def run(self):
         try:
             while True:
-                try:
+                if self.safe:
+                    try:
+                        inp = input('%>').split()
+                        if len(inp) == 0:
+                            continue
+                        command = inp[0]
+                        args = [] if len(inp) == 1 else inp[1:]
+                        res = self.analyzer.run_command(command, args)
+                        if isinstance(res, list):
+                            for obj in res:
+                                print(obj)
+                        else:
+                            print(res)
+                    except Exception as e:
+                        print("Error")
+                        print(e)
+                else:
                     inp = input('%>').split()
                     if len(inp) == 0:
                         continue
@@ -20,9 +37,6 @@ class CLInterface:
                             print(obj)
                     else:
                         print(res)
-                except Exception as e:
-                    print("Error")
-                    print(e)
         except KeyboardInterrupt:
             print("\nExit")
             return
